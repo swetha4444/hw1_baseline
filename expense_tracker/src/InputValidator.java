ceb76261551c292742388986bbf9506657f7a24d
swetha4444 <swethasaseendran4@gmail.com>
2025-03-03 19:55:20 -0500

    fix exception handling

--- expense_tracker/src/InputValidator.java
+++ expense_tracker/src/InputValidator.java
@@ -2,14 +2,10 @@ import java.util.Arrays;
 
 public class InputValidator {
 	public static boolean checkAmountParser(double amount) throws InvalidAmountException {
-		try {
-			if(amount > 0 && amount < 1000)
-				return true;
-			else
+		if(amount < 0 || amount > 1000){
 				throw new InvalidAmountException("Amount not in range");
-		}catch(Exception e) {
-			throw new InvalidAmountException("Amount must be a valid number.");
-		}
+			}
+			return true;
 	}
 	
 	public static boolean checkCategoryParser(String category) throws  InvalidCategoryException{
