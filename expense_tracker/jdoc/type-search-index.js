0e64e9f783501aac1b366cda58e10cce56b4ba04
swetha4444 <swethasaseendran4@gmail.com>
2025-03-03 19:24:31 -0500

    clearing the project

--- expense_tracker/jdoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"ExpenseTrackerApp"},{"p":"<Unnamed>","l":"ExpenseTrackerView"},{"p":"<Unnamed>","l":"Transaction"}];updateSearchResults();
