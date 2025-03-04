33440c76209e158f6615bd98a9654642e011ddb0
swetha4444 <swethasaseendran4@gmail.com>
2025-03-04 15:28:21 -0500

    comments on InputValidator

--- expense_tracker/src/InputValidator.java
+++ expense_tracker/src/InputValidator.java
@@ -1,13 +1,14 @@
 import java.util.Arrays;
 
 public class InputValidator {
+	//Creating Amount Validator
 	public static boolean checkAmountParser(double amount) throws InvalidAmountException {
 		if(amount < 0 || amount > 1000){
 				throw new InvalidAmountException("Amount not in range");
 			}
 			return true;
 	}
-	
+	//Creating Category Validator
 	public static boolean checkCategoryParser(String category) throws  InvalidCategoryException{
 		String[] categories = {"food", "travel", "bills", "entertainment", "other"};
 		if (Arrays.stream(categories).anyMatch(c -> c.equalsIgnoreCase(category))) {
